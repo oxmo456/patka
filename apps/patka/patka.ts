a904fc5a936188046b4bd7d5007858a50db54339
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2026-09-05 12:06:03 -0400

    chore: initialize agent in patka

--- apps/patka/patka.ts
+++ apps/patka/patka.ts
@@ -1,1 +1,13 @@
-export class Patka {}
+import { Ollama } from "ollama";
+import { OllamaInferenceClient } from "./ollama-inference-client.ts";
+import { PatkaAgent } from "./patka-agent.ts";
+
+const MODEL = "qwen2.5-coder:latest";
+
+export class Patka {
+  private readonly patkaAgent: PatkaAgent;
+
+  constructor() {
+    this.patkaAgent = new PatkaAgent(new OllamaInferenceClient(MODEL, new Ollama()));
+  }
+}
